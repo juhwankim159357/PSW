f364bcb779e61ffef2df7f1af77fad2080e71832
Chris Young-Hong <cyoung-hong@myseneca.ca>
2020-12-04 15:12:13 -0500

    reset-password in progress

---
+++ client/src/pages/reset.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class reset extends Component {
+    
+    render() {
+        return (
+            <h3>reset</h3>
+        )
+    }
+}
+
+export default reset
